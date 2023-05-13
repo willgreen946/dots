95b16201f110d324ecce808c1afcad5a03bb902f
ZK <72352225+africavoid@users.noreply.github.com>
2023-05-13 12:31:22 +0000

    XDG_RUNTIME

--- waystart.sh
+++ waystart.sh
@@ -1,6 +1,14 @@
-#pw groupmod video -m user
+#pw groupmod video -m will 
+#pw groupmod audio -m will &
+#pw groupmod adios -m will &
 #pkg install wayland seatd
-export XDG_RUNTIME_DIR=/tmp/xdg-runtime-$(id -u)
+#export XDG_RUNTIME_DIR=/tmp/xdg-runtime-$(id -u)
+#export XDG_RUNTIME_DIR=/run/user/1000
+#export XDG_RUNTIME_DIR=/tmp/$(id -u)
+export XDG_RUNTIME_DIR=$HOME/.tmp/
+#echo "XDG_RUNTIME_DIR=/run/user/$(id -u)" >> ~/.pam_enviroment
+#mkdir -pv ~/.cache/xdgr
+#export XDG_RUNTIME_DIR=$PATH:~/cache/xdgr
 dwl -s somebar
 #sysrc seatd_enable="YES"
 #service seatd start
