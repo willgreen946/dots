57f2bf498179dc657cc5c18932c9069751b3bddb
ZK <72352225+africavoid@users.noreply.github.com>
2023-05-29 20:54:46 +0100

    My config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,14 @@
+config.load_autoconfig(False)
+#dark mode by def
+#config.set("colors.webpage.darkmode.enabled", True)
+
+#fonts
+c.fonts.tabs.selected = '10pt "ubuntu mono"'
+c.fonts.tabs.unselected = '10pt "ubuntu mono"'
+c.fonts.debug_console = '10pt "ubuntu mono"'
+c.fonts.prompts = '10pt "ubuntu mono"'
+c.fonts.statusbar = '10pt "ubuntu mono"'
+
+# binds
+config.bind('P', 'hint links spawn mpv {hint-url}')
+
