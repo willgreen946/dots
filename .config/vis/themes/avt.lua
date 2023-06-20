0d18554532355748705d4b25238e5f1815114e80
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-06-20 11:27:17 +0100

    my vis color scheme

--- .config/vis/themes/avt.lua
+++ .config/vis/themes/avt.lua
@@ -1,1 +1,37 @@
+-- Eight-color scheme
+local lexers = vis.lexers
+-- dark
+lexers.STYLE_DEFAULT ='back:black,fore:white'
+lexers.STYLE_NOTHING = 'back:black'
+lexers.STYLE_CLASS = 'fore:red,bold'
+lexers.STYLE_COMMENT = 'fore:#202020,italics'
+lexers.STYLE_CONSTANT = 'fore:#f26711,bold'
+lexers.STYLE_DEFINITION = 'fore:red,bold'
+lexers.STYLE_ERROR = 'fore:blue,italics'
+lexers.STYLE_FUNCTION = 'fore:red,bold'
+lexers.STYLE_KEYWORD = 'fore:red,bold'
+lexers.STYLE_LABEL = 'fore:red,bold'
+lexers.STYLE_NUMBER = 'fore:#f2ca30'
+lexers.STYLE_OPERATOR = 'fore:orange,bold'
+lexers.STYLE_REGEX = 'fore:red'
+lexers.STYLE_STRING = 'fore:yellow,bold'
+lexers.STYLE_PREPROCESSOR = 'fore:#f26711'
+lexers.STYLE_TAG = 'fore:red,bold'
+lexers.STYLE_TYPE = 'fore:red'
+lexers.STYLE_VARIABLE = 'fore:#eb0028'
+lexers.STYLE_WHITESPACE = ''
+lexers.STYLE_EMBEDDED = 'back:blue,bold'
+lexers.STYLE_IDENTIFIER = 'fore:white'
 
+lexers.STYLE_LINENUMBER = 'fore:white'
+lexers.STYLE_LINENUMBER_CURSOR = lexers.STYLE_LINENUMBER
+lexers.STYLE_CURSOR = 'reverse'
+lexers.STYLE_CURSOR_PRIMARY = lexers.STYLE_CURSOR..',fore:yellow'
+lexers.STYLE_CURSOR_LINE = 'underlined'
+lexers.STYLE_COLOR_COLUMN = 'back:red'
+lexers.STYLE_SELECTION = 'back:white'
+lexers.STYLE_STATUS = 'reverse'
+lexers.STYLE_STATUS_FOCUSED = 'reverse,bold'
+lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
+lexers.STYLE_INFO = 'fore:default,back:default,bold'
+lexers.STYLE_EOF = ''
