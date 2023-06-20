ecc5dc6eed77f30c7ead0cfeed6803152507fb22
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-06-20 11:28:16 +0100

    my vis config

---
+++ .config/vis/visrc.lua
@@ -0,0 +1,13 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+	vis:command('set theme avt')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+	-- Your per window configuration options e.g.
+	-- vis:command('set number')
+	vis:command('set number')
+end)
