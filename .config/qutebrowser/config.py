7fc14295a439f7a3d9d8a55cb42be591f25a2f71
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-06-22 19:37:16 +0100

    my qutebrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,9 +1,8 @@
 config.load_autoconfig(False)
-#dark mode by def
-#config.set("colors.webpage.darkmode.enabled", True)
 
-#fonts
-# binds
+config.set("colors.webpage.darkmode.enabled", True)
+
+# fonts
 c.fonts.completion.entry = "8pt terminus"
 c.fonts.completion.category = "bold 8pt terminus"
 c.fonts.debug_console = "8pt terminus"
@@ -21,6 +20,44 @@ c.fonts.web.family.fixed = "terminus"
 c.fonts.web.family.sans_serif = "terminus"
 c.fonts.web.family.serif = "terminus"
 
+# dimensions
+config.set("completion.height", "25%")
+
+# colors
+config.set("colors.tabs.bar.bg", "#000000")
+config.set("colors.tabs.selected.even.bg", "#000000")
+config.set("colors.tabs.selected.even.fg", "#f26711")
+config.set("colors.tabs.even.bg", "#000000")
+config.set("colors.tabs.even.fg", "#ffffff")
+config.set("colors.tabs.selected.odd.fg", "#f26711")
+config.set("colors.tabs.selected.odd.bg", "#000000")
+config.set("colors.tabs.odd.fg", "#ffffff")
+config.set("colors.tabs.odd.bg", "#000000")
+config.set("colors.hints.fg", "#f26711")
+config.set("colors.hints.bg", "#000000")
+config.set("colors.statusbar.caret.fg", "#f26711")
+config.set("colors.statusbar.caret.bg", "#000000")
+config.set("colors.statusbar.caret.selection.fg", "#f26711")
+config.set("colors.statusbar.caret.selection.bg", "#000000")
+config.set("colors.statusbar.command.fg", "#f26711")
+config.set("colors.statusbar.command.bg", "#000000")
+config.set("colors.statusbar.insert.fg", "#f26711")
+config.set("colors.statusbar.insert.bg", "#000000")
+config.set("colors.prompts.bg", "#000000")
+config.set("colors.prompts.fg", "#f26711")
+config.set("colors.contextmenu.menu.fg", "#f26711")
+config.set("colors.contextmenu.menu.bg", "#000000")
+config.set("colors.completion.category.fg", "#f26711")
+config.set("colors.completion.category.bg", "#000000")
+config.set("colors.completion.fg",	'#f26711')
+config.set("colors.completion.even.bg", "#000000")
+config.set("colors.completion.item.selected.bg", "#000000")
+config.set("colors.completion.item.selected.fg", "#f26711")
+config.set("colors.completion.item.selected.border.top", "#f26711")
+config.set("colors.completion.item.selected.border.bottom", "#f26711")
+config.set("colors.completion.odd.bg", "#000000")
+config.set("colors.completion.fg", "#f26711")
+# binds
 config.bind('M', 'hint links spawn mpv {hint-url}')
 config.bind('W', 'hint links download')
 config.bind('d', 'tab-close')
@@ -28,3 +65,8 @@ config.bind('J', 'tab-prev')
 config.bind('K', 'tab-next')
 config.bind('L', 'forward')
 config.bind('H', 'back')
+
+# vifm as file browser
+config.set("fileselect.handler", "external")
+config.set("fileselect.single_file.command", ['st', 'vifm', '--choose-files', '{}'])
+config.set("fileselect.multiple_files.command", ['st', 'vifm', '--choose-files', '{}'])
